d3e61afa7893ae14ac271d84c878d4e3ca7df9d8
Luffy <kosseyla.hamouche@gmail.com>
2018-11-25 19:49:33 +0100

    update js exercices

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,31 +1,73 @@
-function ucfirst(string) {
-	return string.charAt(0).toUppercase()+string.slice(1);
+function isEmpty(string){
+    if(!string || typeof string != "string" || string == ""){
+        return true;
+    }
+    return false;
 }
 
-function capitalize(string) {
-	let str = string.slit(" ");
-	let strf = "";
-
-	for (let test in str) {
-		strf += str[test].charAt(0).toUpperCase()+str[test].slice(1)+" ";
-	}
-	return strf;
+function ucFirst(string){
+    if(isEmpty(string)){
+        return "Empty string"
+    }
+    const c = string[0].toUpperCase();
+    string = string.substring(1);
+    string = c + string;
+    return string;
 }
 
-function camelCase(string) {
-	let str = string.slit(" ");
-	let strf = "";
-
-	for (let test in str) {
-		strf += str[test].charAt(0).toUpperCase()+str[test].slice(1);
-	}
-	return strf;
+function capitalize(string, separator){
+    if(isEmpty(string)){
+        return "Empty string"
+    }
+    let str = string.split(" ");
+    let strf = [];
+    for(let test of str){
+        strf.push(ucFirst(test))
+    }
+    return strf.join(separator);
 }
 
-function snake_case(string) {
-	return (string.split(' ').join('_'));
+function camelCase(string){
+    if(isEmpty(string)){
+        return "Empty"
+    }
+    return capitalize(string, "");
 }
 
-function leet(string) {
-	
+function snake_case(string){
+    if(isEmpty(string)){
+        return "Empty"
+    }
+    let str = string.split(" ");
+    return str.join("_");
+}
+
+function leet(string){
+    if(isEmpty(string)){
+        return "Empty"
+    }
+    let letters = string.split("");
+    for(var i=0;i<letters.length;i++){
+        switch(letters[i].toLowerCase()){
+            case "a":
+            letters[i] = "4";
+            break;
+            case "e":
+            letters[i] = "3";
+            break;
+            case "i":
+            letters[i] = "1";
+            break;
+            case "o":
+            letters[i] = "0";
+            break;
+            case "u":
+            letters[i] = "(_)";
+            break;
+            case "y":
+            letters[i] = "7";
+            break;
+        }
+    }
+    return letters.join("");
 }
