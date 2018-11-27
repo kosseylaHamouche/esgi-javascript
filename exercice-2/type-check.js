570cc68d7d8b9b19ed38a6f3b331e51bb99b9a71
Luffy <kosseyla.hamouche@gmail.com>
2018-11-27 18:25:42 +0100

    exercice 2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,17 +1,20 @@
 function type_check_v1(param, type){
-	if(typeof param === "number" && typeof type === "number") {
+	if(typeof param === "number" && type === "number") {
 		return true;
 	}
-	else if(typeof param === "string" && typeof type === "string") {
+	else if(typeof param === "string" && type === "string") {
 		return true;
 	}
-	else if(typeof param === "number" && typeof type === "number") {
+	else if(typeof param === "number" && type === "number") {
 		return true;
 	}
-	else if(typeof param === "boolean" && typeof type === "boolean") {
+	else if(typeof param === "boolean" && type === "boolean") {
 		return true;
 	}
-	else if(typeof param === "object" && param != null && typeof type === "object") {
+	else if(typeof param === "object" && param != null && type === "object") {
+		return true;
+	}
+	else if(typeof param === "object" && type === "object") {
 		return true;
 	}
 	else {
