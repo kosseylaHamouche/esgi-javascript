4b364b379be047ce5d9e4e7abff850ef2f3e4c94
Luffy <kosseyla.hamouche@gmail.com>
2018-11-27 19:11:59 +0100

    [DONE] exercice 2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -2,11 +2,10 @@ function type_check_v1(param, type){
 	if(typeof param === type) {
 		return true;
 	}
+	else if (Array.isArray(param) && type ==='array'){
+		return true;
+	}
 	else {
 		return false;
 	}
-}
-
-function type_check_v2(){
-
 }
